4699d0ba9a6ad74901f198b0304bd8d9061c1aaf
Christian Piper <xmanpiper@gmail.com>
2023-11-30 16:26:31 -0500

    Renamed S3 bucket and added more headers to Lambdas

--- backend/libs/htmlResponses.ts
+++ backend/libs/htmlResponses.ts
@@ -2,7 +2,9 @@ export function successResponse(message: string) {
     return {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*", //DO NOT USE THIS VALUE IN PRODUCTION - https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-cors.html
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
         body: message,
     };
@@ -12,7 +14,9 @@ export function errorResponse(message: string) {
     return {
         statusCode: 400,
         headers: {
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*", //DO NOT USE THIS VALUE IN PRODUCTION - https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-cors.html
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
         },
         body: message,
     };
