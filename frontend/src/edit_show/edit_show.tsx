14829c5df6240cc40f83a5301cb6f024e4ab9084
Rosaline <rosaline.y.guo@gmail.com>
2023-12-14 23:12:42 -0500

    Implemented Create/Delete/List blocks information exchange. TODO: Get the information displayed onto GUI for user feedback, add error checking so that silly TAs don't make mistakes

--- frontend/src/edit_show/edit_show.tsx
+++ frontend/src/edit_show/edit_show.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from "react";
-import { instance, getInput } from "../main";
+import { instance, getInput, getSelect } from "../main";
 import { getPassword, getUsername } from "../useLogin";
 import { useLocation } from "react-router-dom";
 
 export default function EditShow() {
     const location = useLocation();
+    let sectionInfos = new Array();
 
     function createShow() {
         //TODO: pass info to backend about the show
@@ -53,6 +54,17 @@ export default function EditShow() {
         const email = getUsername();
         const password = getPassword();
         const blockName = getInput("block-name")
+        const blockPrice = getInput("block-price")
+        const sectionName = getSelect("section")
+        const startRow = Number(getInput("start-row"))
+        const endRow = Number(getInput("end-row"))
+        const seats = new Array();
+        const selectedSectionInfo = sectionInfos.filter((value)=>{return value.name === sectionName})[0]; //TODO
+        for (let i = startRow; i <= endRow; i++){
+            for (let j = 0; j < selectedSectionInfo.cols; j++){
+                seats.push({"section":selectedSectionInfo.index, "row":i, "column":j});
+            }
+        }
 
         const data = {
             email: email,
@@ -61,7 +73,7 @@ export default function EditShow() {
             show: location.state.show,
             time: new Date(location.state.time),
             name: blockName,
-            price: showPrice,
+            price: blockPrice,
             seats: seats,
         };
 
@@ -78,6 +90,7 @@ export default function EditShow() {
     function deleteBlock(){
         const email = getUsername();
         const password = getPassword();
+        const blockName = getInput("block-name")
 
         const data = {
             email: email,
@@ -85,8 +98,8 @@ export default function EditShow() {
             venue: location.state.venue,
             show: location.state.show,
             time: new Date(location.state.time),
-            price: showPrice,
-            seats: seats,
+            name: blockName
+            
         };
 
         instance
@@ -116,9 +129,45 @@ export default function EditShow() {
             .post("/list-blocks", data)
             .then((response) => {
                 console.log(response);
-                for(const section of response.data.seats){
+                const selectDropdown = document.getElementById('section') as HTMLSelectElement;
+                selectDropdown.innerHTML = "";
+                const uniqueBlockNames = new Array();
+                let a = 0;
+                for(const section of response.data){
+                    if(sectionInfos.length < 3){
+                        sectionInfos.push({"name" : section.name, "rows" : section.rows, "cols" : section.columns, "index" : a})
+                        a += 1;
+                    }
+                    const nameElement = document.createElement('option') as HTMLOptionElement;
                     console.log(section);
+                    nameElement.text = section.name;
+                    selectDropdown.add(nameElement);
+                    for(const seat of section.seats){
+                        // if(){
+                            // console.log(uniqueBlockNames);
+                            // console.log(seat);
+                            const selectedBlock = uniqueBlockNames.filter((value)=>{return value.blockname.toString() === seat.block.name.toString()})
+                            if(seat.block.name == "Default"){
+                                continue;
+                            }
+                            if(selectedBlock.length <= 0){
+                                uniqueBlockNames.push({"blockname": seat.block.name,
+                                    "price":seat.block.price,
+                                    "section":section.name,
+                                    "startrow":seat.row,
+                                    "endrow":seat.row});
+                            }
+                            else{
+                                uniqueBlockNames[uniqueBlockNames.reduce((arr, e, i) => ((e.blockname === seat.block.name) && arr.push(i), arr), [])]= {...uniqueBlockNames[uniqueBlockNames.reduce((arr, e, i) => ((e.blockname === seat.block.name) && arr.push(i), arr), [])], "endrow":seat.row};
+                            }
+                        // }
+                    }
                 }
+                console.log(sectionInfos)
+                console.log(uniqueBlockNames);
+                const table = document.getElementById('block-listings') as HTMLTableElement;
+                // for(const name in uniqueBlockNames){}
+                // table.insertRow().insertCell(0);
             })
             .catch((error) => {
                 console.log(error);
@@ -126,39 +175,37 @@ export default function EditShow() {
 
 
     }
-    function updateShow(){
-
-        
-        
-    
-    }
-
-    useEffect(() => {
-        (document.getElementById('show-date') as HTMLInputElement).placeholder = (new Date(location.state.time)).toDateString();
-        (document.getElementById('show-time') as HTMLInputElement).placeholder = (new Date(location.state.time)).toTimeString();
-        (document.getElementById('show-name') as HTMLInputElement).placeholder = location.state.show;
-        //(document.getElementById('price') as HTMLInputElement).value = 
-
-        //first -- 
-
-        //add options to sections
-
-
-    })
+    useEffect( () => {
+        // listBlocks();
+    }, []);
 
     return (
         <>
             <div>
                 <h1>Edit Show:</h1>
-                <table>
-                    <tr></tr>
-                    <tr></tr>
-
-                </table>
                 <select id="section"/>
-                <select id="start-row"/>
-                <select id="end-row"/>
+                <input type="number" placeholder="Starting row..." id="start-row"/>
+                <input type="number" placeholder="Ending row..." id="end-row"/>
+                
+                Block Name:
+                <input className="blockName" type="text" id="block-name" />
 
+                Block Price:
+                <input className="blockPrice" type="number" min="0" step=".01" id="block-price"/>
+
+                <button className="createBloclButton" onClick={() => createBlock()}>
+                    Create Block
+                </button>
+
+                <button className="deleteBlockButton" onClick={() => deleteBlock()}>
+                    Delete Block
+                </button>
+
+                <button className="listBlocksButton" onClick={() => listBlocks()}>
+                    List Blocks
+                </button>
+
+                <table id="block-listings"></table>
             </div>
         </>
     );
